a1391d0ae207df1db13e3c81b3ff4fcf119c891c
748748748 <james.tps09@gmail.com>
2021-01-05 17:36:15 +0800

    Add existing file

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/apex-15/catkin_ws/src'
 whitelisted_packages = 'rqt_diagnostic_exp'.split(';') if 'rqt_diagnostic_exp' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
+underlay_workspaces = '/home/apex-15/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/apex-15/catkin_ws/devel;/opt/ros/melodic' != '' else []
